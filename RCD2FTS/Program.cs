f3d1cda63b14205a9f0d3d72d05fc1eb4e19eadf
mjmazur <mmazur5@uwo.ca>
2021-04-16 12:58:24 -0400

    Created RCD2FTS VS project

---
+++ RCD2FTS/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RCD2FTS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
